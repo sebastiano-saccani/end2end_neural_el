7df11ed47cbdb3f36da647036ead3d83a53715bd
SebastianoSaccani <sebastiano.saccani@gmail.com>
2020-03-13 16:53:31 +0100

    mod for docker compatibility

--- code/model/config.py
+++ code/model/config.py
@@ -1,4 +1,6 @@
-base_folder = "../"
+from pathlib import Path
+base_folder = str(Path(__file__).parent.parent.parent).rstrip("/") + "/"  # Ensure there's at least a / at the end
+
 spans_separators = ["."]  #maybe also try ['.', ',', ';']
 
 unk_ent_id = "0"
